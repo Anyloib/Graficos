b261cc06b8f1519832ea5735d467f810a8191f35
alumno <alumno@D3A06>
2018-01-22 10:51:21 -0700

    Dibujo de un triangulo

--- Graficos/Graficos.cpp
+++ Graficos/Graficos.cpp
@@ -28,7 +28,19 @@ void actualizar()
 
 void dibujar()
 {
+	//Inicia la rutina con un modo de dibujo
+	glBegin(GL_TRIANGLES);
 
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 1.0f);
+
+	glEnd();
 }
 
 int main()
